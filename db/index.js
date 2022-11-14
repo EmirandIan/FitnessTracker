694f207156efdf9c6c6b79d1d1b2ff392fab99d1
emirt <emirtaletovic22@gmail.com>
2022-11-14 15:24:42 -0600

    test

--- db/index.js
+++ db/index.js
@@ -1,1 +1,9 @@
-console.log("This real index");
+const { Client } = require('pg')
+
+
+//module.exports = {
+    // ...require('./client'), // adds key/values from users.js
+  //  ...require('./users'), // adds key/values from users.js
+    //...require('./activities'), // adds key/values from activites.js
+    //...require('./routines'), // etc
+    //...require('./routine_activities') // etc
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
-const pg = require('pg');
+const express = require('express');
+const router = express.Router();
 
-const client = new pg.Client(`postgres://localhost:5432/fitness-dev`);
+// GET /api/health
+router.get('/health', async (req, res, next) => {
+});
 
-module.exports = {
-    client
-}
+// ROUTER: /api/users
+const usersRouter = require('./users');
+router.use('/users', usersRouter);
+
+// ROUTER: /api/activities
+const activitiesRouter = require('./activities');
+router.use('/activities', activitiesRouter);
+
+// ROUTER: /api/routines
+const routinesRouter = require('./routines');
+router.use('/routines', routinesRouter);
+
+// ROUTER: /api/routine_activities
+const routineActivitiesRouter = require('./routineActivities');
+router.use('/routine_activities', routineActivitiesRouter);
+
+module.exports = router;

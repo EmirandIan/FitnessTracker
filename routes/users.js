c24d31141de297cd2f96e3fd88a74eba1c2487f7
emirt <emirtaletovic22@gmail.com>
2022-11-16 12:40:27 -0600

    routes incoming

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,77 @@
 const express = require('express');
 const router = express.Router();
 
-// POST /api/users/login
 
-// POST /api/users/register
-
-// GET /api/users/me
-
-// GET /api/users/:username/routines
-
-module.exports = router;
+usersRouter.use((req, res, next) => {
+    console.log("A request is being made to /users");
+  
+    next(); // THIS IS DIFFERENT
+  });
+  
+  const { getUserByUsername } = require('../db');
+  
+  // UPDATE
+  usersRouter.post('/login', async (req, res, next) => {
+      const { username, password } = req.body;
+    
+      // request must have both
+      if (!username || !password) {
+        next({
+          name: "MissingCredentialsError",
+          message: "Please supply both a username and password"
+        });
+      }
+    
+      try {
+        const user = await getUserByUsername(username);
+    
+        if (user && user.password == password) {
+          // create token & return to user
+          res.send({ message: "you're logged in!" });
+        } else {
+          next({ 
+            name: 'IncorrectCredentialsError', 
+            message: 'Username or password is incorrect'
+          });
+        }
+      } catch(error) {
+        console.log(error);
+        next(error);
+      }
+    });
+    usersRouter.post('/register', async (req, res, next) => {
+      const { username, password, name, location } = req.body;
+    
+      try {
+        const _user = await getUserByUsername(username);
+    
+        if (_user) {
+          next({
+            name: 'UserExistsError',
+            message: 'A user by that username already exists'
+          });
+        }
+    
+        const user = await createUser({
+          username,
+          password,
+          name,
+          location,
+        });
+    
+        const token = jwt.sign({ 
+          id: user.id, 
+          username
+        }, process.env.JWT_SECRET, {
+          expiresIn: '1w'
+        });
+    
+        res.send({ 
+          message: "thank you for signing up",
+          token 
+        });
+      } catch ({ name, message }) {
+        next({ name, message })
+      } 
+    });
+  module.exports = router;
